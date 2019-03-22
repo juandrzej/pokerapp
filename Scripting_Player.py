94f79798fdead06e3dba59eac6128f492b78326f
juandrzej <ziebandrzej@gmail.com>
2019-03-22 11:38:15 +0100

    scritpitng played kinda finished

--- Scripting_Player.py
+++ Scripting_Player.py
@@ -73,32 +73,12 @@ class Scripting_Player(Player):
     @property
     def preflop(self):
         reactions = {self.value_6Bet: self.Six_Bet_Call,
-                     self.value_5Bet: self.BB50,
-                     self.value_4Bet: self.BB25,
-                     self.value_3Bet: self.BB10,
-                     self.value_OPR: self.BB3,
+                     self.value_5Bet: self.Six_Bet_Raise,
+                     self.value_4Bet: self.self.Five_Bet_Raise,
+                     self.value_3Bet: self.Four_Bet_Raise,
+                     self.value_OPR: self.Three_Bet_Raise,
                      self.game.BB: self.OP_Raise}
         return reactions[self.game.current_raise]()
-
-    def BB50(self):
-        self.Five_Bet_Call()
-        if self.action:
-            self.Six_Bet_Raise()
-
-    def BB25(self):
-        self.Four_Bet_Call()
-        if self.action:
-            self.Five_Bet_Raise()
-
-    def BB10(self):
-        self.Three_Bet_Call()
-        if self.action:
-            self.Four_Bet_Raise()
-
-    def BB3(self):
-        self.OPR_Call()
-        if self.action:
-            self.Three_Bet_Raise()
 
     def OPR_Call(self):
         OPRsCall = [self.Button_Call, self.SB_Call, self.BB_Call,
@@ -111,245 +91,173 @@ class Scripting_Player(Player):
         return OPRs[self.position]()
 
     def UTG_Raise(self):
-        if self.pocket_pair:
-            self.open_raise(self.value_OPR)
-        elif self.higher(14) and self.lower(10):
-            if self.suited or self.lower(11):
-                self.open_raise(self.value_OPR)
-            else:
-                self.fold
-        elif self.connectors(12):
+        range = [self.pocket_pair, self.connectors(12),
+                 all[self.higher(14), self.lower(10), self.suited],
+                 all[self.higher(14), self.lower(11)]]
+        if any(range):
             self.open_raise(self.value_OPR)
         else:
             self.fold
 
     def MP_Raise(self):
-        if self.pocket_pair:
-            self.open_raise(self.value_OPR)
-        elif self.higher(14) and self.lower(10):
-            self.open_raise(self.value_OPR)
-        elif self.higher(13) and self.lower(11):
+        range = [self.pocket_pair,
+                 all[self.higher(14), self.lower(10)],
+                 all[self.higher(13), self.lower(11)]]
+        if any(range):
             self.open_raise(self.value_OPR)
         else:
             self.fold
 
     def Cut_Off_Raise(self):
-        if self.pocket_pair:
-            self.open_raise(self.value_OPR)
-        elif self.higher(14) and self.lower(6):
-            if self.suited or self.lower(9):
-                self.open_raise(self.value_OPR)
-            else:
-                self.fold
-        elif self.higher(10) and self.lower(9):
-            self.open_raise(self.value_OPR)
-        elif self.suited_one_gappers(8):
-            self.open_raise(self.value_OPR)
-        elif self.suited_connectors(5):
-            self.open_raise(self.value_OPR)
-        elif self.connectors(7):
+        range = [self.pocket_pair, self.suited_connectors(5),
+                 self.suited_one_gappers(8), self.connectors(7),
+                 all[self.higher(14), self.lower(6), self.suited],
+                 all[self.higher(14), self.lower(9)],
+                 all[self.higher(10), self.lower(9)]]
+        if any(range):
             self.open_raise(self.value_OPR)
         else:
             self.fold
 
     def Button_Raise(self):
-        if self.pocket_pair:
-            self.open_raise(self.value_OPR)
-        elif self.higher(14):
-            self.open_raise(self.value_OPR)
-        elif self.higher(11) and self.lower(7):
-            if self.suited or self.lower(8):
-                self.open_raise(self.value_OPR)
-            else:
-                self.fold
-        elif self.one_gappers(7):
-            self.open_raise(self.value_OPR)
-        elif self.connectors(4):
+        range = [self.pocket_pair, self.higher(14),
+                 all[self.higher(11), self.lower(7), self.suited],
+                 all[self.higher(11), self.lower(8)],
+                 self.connectors(4), self.one_gappers(7)]
+        if any(range):
             self.open_raise(self.value_OPR)
         else:
             self.fold
 
     def SB_Raise(self):
-        if self.pocket_pair:
-            self.open_raise(self.value_OPR)
-        elif self.higher(14) and self.lower(6):
-            if self.suited or self.lower(10):
-                self.open_raise(self.value_OPR)
-            else:
-                self.fold
-        elif self.higher(11) and self.lower(8):
-            if self.suited or self.lower(9):
-                self.open_raise(self.value_OPR)
-            else:
-                self.fold
-        elif self.suited_connectors(6):
+        range = [self.pocket_pair, self.suited_connectors(6),
+                 all[self.higher(14), self.lower(6), self.suited],
+                 all[self.higher(14), self.lower(10)],
+                 all[self.higher(11), self.lower(8), self.suited],
+                 all[self.higher(11), self.lower(9)]]
+        if any(range):
             self.open_raise(self.value_OPR)
         else:
             self.fold
 
     def BB_Raise(self):
-        if self.pocket_pair:
-            self.open_raise(self.value_OPR)
-        elif self.higher(14):
-            self.open_raise(self.value_OPR)
-        elif self.higher(11) and self.lower(7):
-            if self.suited or self.lower(8):
-                self.open_raise(self.value_OPR)
-            else:
-                self.fold
-        elif self.one_gappers(7):
-            self.open_raise(self.value_OPR)
-        elif self.connectors(4):
+        range = [self.pocket_pair, self.higher(14),
+                 all[self.higher(11), self.lower(7), self.suited],
+                 all[self.higher(11), self.lower(8)],
+                 self.connectors(4), self.one_gappers(7)]
+        if any(range):
             self.open_raise(self.value_OPR)
         else:
             self.fold
 
     def UTG_Call(self):
-        if self.pocket_pair:
-            self.call
-        elif self.higher(14) and self.lower(10):
-            if self.suited or self.lower(11):
-                self.call
-            else:
-                self.fold
-        elif self.connectors(12):
+        range = [self.pocket_pair, self.connectors(12),
+                 all[self.higher(14), self.lower(10), self.suited],
+                 all[self.higher(14), self.lower(11)]]
+        if any(range):
             self.call
         else:
             self.fold
 
     def MP_Call(self):
-        if self.pocket_pair:
-            self.call
-        elif self.higher(14) and self.lower(10):
-            self.call
-        elif self.higher(13) and self.lower(11):
+        range = [self.pocket_pair,
+                 all[self.higher(14), self.lower(10)],
+                 all[self.higher(13), self.lower(11)]]
+        if any(range):
             self.call
         else:
             self.fold
 
     def Cut_Off_Call(self):
-        if self.pocket_pair:
-            self.call
-        elif self.higher(14) and self.lower(6):
-            if self.suited or self.lower(9):
-                self.call
-            else:
-                self.fold
-        elif self.higher(10) and self.lower(9):
-            self.call
-        elif self.suited_one_gappers(8):
-            self.call
-        elif self.suited_connectors(5):
-            self.call
-        elif self.connectors(7):
+        range = [self.pocket_pair, self.suited_connectors(5),
+                 self.suited_one_gappers(8), self.connectors(7),
+                 all[self.higher(14), self.lower(6), self.suited],
+                 all[self.higher(14), self.lower(9)],
+                 all[self.higher(10), self.lower(9)]]
+        if any(range):
             self.call
         else:
             self.fold
 
     def Button_Call(self):
-        if self.pocket_pair:
-            self.call
-        elif self.higher(14):
-            self.call
-        elif self.higher(11) and self.lower(7):
-            if self.suited or self.lower(8):
-                self.call
-            else:
-                self.fold
-        elif self.connectors(4):
-            self.call
-        elif self.one_gappers(7):
+        range = [self.pocket_pair, self.higher(14),
+                 all[self.higher(11), self.lower(7), self.suited],
+                 all[self.higher(11), self.lower(8)],
+                 self.connectors(4), self.one_gappers(7)]
+        if any(range):
             self.call
         else:
             self.fold
 
     def SB_Call(self):
-        if self.pocket_pair:
-            self.call
-        elif self.higher(14) and self.lower(6):
-            if self.suited or self.lower(10):
-                self.call
-            else:
-                self.fold
-        elif self.higher(11) and self.lower(8):
-            if self.suited or self.lower(9):
-                self.call
-            else:
-                self.fold
-        elif self.suited_connectors(6):
+        range = [self.pocket_pair, self.suited_connectors(6),
+                 all[self.higher(14), self.lower(6), self.suited],
+                 all[self.higher(14), self.lower(10)],
+                 all[self.higher(11), self.lower(8), self.suited],
+                 all[self.higher(11), self.lower(9)]]
+        if any(range):
             self.call
         else:
             self.fold
 
     def BB_Call(self):
-        if self.pocket_pair:
-            self.call
-        elif self.higher(14):
-            self.call
-        elif self.higher(11) and self.lower(7):
-            if self.suited or self.lower(8):
-                self.call
-            else:
-                self.fold
-        elif self.connectors(4):
-            self.call
-        elif self.one_gappers(7):
+        range = [self.pocket_pair, self.higher(14),
+                 all[self.higher(11), self.lower(7), self.suited],
+                 all[self.higher(11), self.lower(8)],
+                 self.connectors(4), self.one_gappers(7)]
+        if any(range):
             self.call
         else:
             self.fold
 
     def Three_Bet_Raise(self):
-        if self.pocket_pair_higher(7):
-            self.re_raise(self.value_3Bet)
-        elif self.higher(14) and self.lower(11):
-            if self.suited or self.lower(12):
+        self.OPR_Call()
+        if self.action:
+            range = [self.pocket_pair_higher(7),
+                     all[self.higher(14), self.lower(11), self.suited],
+                     all[self.higher(14), self.lower(12)],
+                     self.suited_connectors(12)]
+            if any(range):
                 self.re_raise(self.value_3Bet)
             else:
                 self.check
-        elif self.suited_connectors(12):
-            self.re_raise(self.value_3Bet)
-        else:
-            self.check
 
     def Three_Bet_Call(self):
-        if self.pocket_pair_higher(7):
-            self.re_call
-        elif self.higher(14) and self.lower(11):
-            if self.suited or self.lower(12):
-                self.re_call
-            else:
-                self.fold
-        elif self.suited_connectors(12):
+        range = [self.pocket_pair_higher(7),
+                 all[self.higher(14), self.lower(11), self.suited],
+                 all[self.higher(14), self.lower(12)],
+                 self.suited_connectors(12)]
+        if any(range):
             self.re_call
         else:
             self.fold
 
     def Four_Bet_Raise(self):
-        if self.pocket_pair_higher(11):
-            self.re_raise(self.value_4Bet)
-        elif self.connectors(13):
-            self.re_raise(self.value_4Bet)
-        else:
-            self.check
+        self.Three_Bet_Call()
+        if self.action:
+            range = [self.pocket_pair_higher(11), self.connectors(13)]
+            if any(range):
+                self.re_raise(self.value_4Bet)
+            else:
+                self.check
 
     def Four_Bet_Call(self):
-        if self.pocket_pair_higher(10):
+        range = [self.pocket_pair_higher(10),
+                 all[self.higher(14), self.lower(12), self.suited],
+                 all[self.higher(14), self.lower(13)]]
+        if any(range):
             self.re_call
-        elif self.higher(14) and self.lower(12):
-            if self.suited or self.lower(13):
-                self.re_call
-            else:
-                self.fold
         else:
             self.fold
 
     def Five_Bet_Raise(self):
-        if self.pocket_pair_higher(12):
-            self.re_raise(self.value_5Bet)
-        elif self.suited_connectors(13):
-            self.re_raise(self.value_5Bet)
-        else:
-            self.check
+        self.Four_Bet_Call()
+        if self.action:
+            range = [self.pocket_pair_higher(12), self.suited_connectors(13)]
+            if any(range):
+                self.re_raise(self.value_5Bet)
+            else:
+                self.check
 
     def Five_Bet_Call(self):
         if self.pocket_pair_higher(13):
@@ -358,10 +266,12 @@ class Scripting_Player(Player):
             self.fold
 
     def Six_Bet_Raise(self):
-        if self.pocket_pair_higher(14):
-            self.re_raise(self.value_6Bet)
-        else:
-            self.check
+        self.Five_Bet_Call()
+        if self.action:
+            if self.pocket_pair_higher(14):
+                self.re_raise(self.value_6Bet)
+            else:
+                self.check
 
     def Six_Bet_Call(self):
         if self.pocket_pair_higher(14):
